66f802230f419c4cee42852cf37ae6d919397ade
namNguyenThanh1511 <nam151103@gmail.com>
2025-11-10 12:39:01 +0700

    fix

--- HeartSpace.Api/Controllers/PaymentRequestController.cs
+++ HeartSpace.Api/Controllers/PaymentRequestController.cs
@@ -40,22 +40,15 @@ namespace HeartSpace.Api.Controllers
             var updatedRequest = await _paymentRequestService.UpdatePaymentRequest(paymentRequest);
             return Ok("Update succeed");
         }
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePaymentRequest(Guid id)
-        {
-            var result = await _paymentRequestService.DeletePaymentRequest(id);
-            if (!result)
-                return NotFound();
-            return NoContent();
-        }
+
         [HttpGet("by-appointment/{appointmentId}")]
-        public async Task<ActionResult<ApiResponse<PaymentRequestResponse>>> GetPaymentRequestsByAppointmentId(Guid appointmentId)
+        public async Task<ActionResult<ApiResponse<List<PaymentRequestResponse>>>> GetPaymentRequestsByAppointmentId(Guid appointmentId)
         {
             var paymentRequests = await _paymentRequestService.GetPaymentRequestByAppointmentId(appointmentId);
             return Ok(paymentRequests);
         }
         [HttpGet("by-consultant/{consultantId}")]
-        public async Task<ActionResult<ApiResponse<PaymentRequestResponse>>> GetPaymentRequestsByConsultantId(Guid consultantId)
+        public async Task<ActionResult<ApiResponse<List<PaymentRequestResponse>>>> GetPaymentRequestsByConsultantId(Guid consultantId)
         {
             var paymentRequests = await _paymentRequestService.GetPaymentRequestByConsultantId(consultantId);
             return Ok(paymentRequests);
